3478632cf36a4a4bd282979f40bd8a0af914f25c
Worthington <mearmour@uark.edu>
2020-04-08 18:37:20 -0500

    corrected script

--- assn06/parseGFF.py
+++ assn06/parseGFF.py
@@ -3,23 +3,17 @@ import argparse
 from Bio import SeqIO
 
 parser = argparse.ArgumentParser()
-parser.add_argument("*.gff")
-parser.add_argument("*.fasta")
+parser.add_argument("gff")
+parser.add_argument("fasta")
 args = parser.parse_args()
-genome = SeqIO.read(args.fasta, "*.fasta")
+watermelon = SeqIO.read(args.fasta, "fasta")
 with open(args.gff, 'r') as gff_file:
 	reader = csv.reader(gff_file, delimiter="\t")
 	for line in reader:
-		columns = line.rstrip("\n").split(",")
-		gene = columns[8]
-		if gene == gene and not gene.startswith("Gene similar"):
-			gene2 = gene[5:9]
-			reader2.write(gene2 + "\n")
+		if not line:
+			continue
 		else:
-			pass
-	reader2.close()
-	
-	sorted_list = sorted(reader2)
-	for line in sorted_list:
-		alphabetized_list = line.rstrip("\n")
-		print(alphabetized_list)
+			start_coordinate = line[3]
+			end_coordinate = line[4]
+			print(start_coordinate, end_coordinate)
+gene_names = []
